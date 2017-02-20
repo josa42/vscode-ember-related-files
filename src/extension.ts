90f282f360f697b5c2be876d9d71e6f3d61e913d
Josa Gesell <josa@gesell.me>
2017-02-20 20:33:26 +0100

    📦  Small refactoring

--- src/extension.ts
+++ src/extension.ts
@@ -16,39 +16,39 @@ const groups = [
 ]
 
 const types = [
-  { module: 'component',              exp: /^(app|addon)\/components\/(.+)\.(js)$/ },
-  { module: 'component-template',     exp: /^(app|addon)\/templates\/components\/(.+)\.(hbs)$/ },
-  { module: 'component-style',        exp: /^(app|addon)\/styles\/components\/(.+)\.(scss)$/ },
-  { module: 'component-unit',         exp: /^()tests\/unit\/components\/(.+)-test\.(js)$/ },
-  { module: 'component-integration',  exp: /^()tests\/integration\/components\/(.+)-test\.(js)$/ },
-  { module: 'route',                  exp: /^(app|addon)\/routes\/(.+)\.(js)$/ },
-  { module: 'route-unit',             exp: /^()tests\/unit\/routes\/(.+)-test\.(js)$/ },
-  { module: 'route-integration',      exp: /^()tests\/integration\/routes\/(.+)-test\.(js)$/ },
-  { module: 'controller',             exp: /^(app|addon)\/controllers\/(.+)\.(js)$/ },
-  { module: 'controller-unit',        exp: /^()tests\/unit\/controllers\/(.+)-test\.(js)$/ },
-  { module: 'controller-integration', exp: /^()tests\/integration\/controllers\/(.+)-test\.(js)$/ },
-  { module: 'controller-template',    exp: /^(app|addon)\/templates\/(.+)\.(hbs)$/ },
-  { module: 'model',                  exp: /^(app|addon)\/models\/(.+)\.(js)$/ },
-  { module: 'model-unit',             exp: /^()tests\/unit\/models\/(.+)-test\.(js)$/ },
-  { module: 'model-integration',      exp: /^()tests\/integration\/models\/(.+)-test\.(js)$/ },
-  { module: 'util',                   exp: /^(app|addon)\/utils\/(.+)\.(js)$/ },
-  { module: 'util-unit',              exp: /^()tests\/unit\/utils\/(.+)-test\.(js)$/ },
-  { module: 'util-integration',       exp: /^()tests\/integration\/utils\/(.+)-test\.(js)$/ },
-  { module: 'helper',                 exp: /^(app|addon)\/helpers\/(.+)\.(js)$/ },
-  { module: 'helper-unit',            exp: /^()tests\/unit\/helpers\/(.+)-test\.(js)$/ },
-  { module: 'helper-integration',     exp: /^()tests\/integration\/helpers\/(.+)-test\.(js)$/ },
-  { module: 'mixin',                  exp: /^(app|addon)\/mixins\/(.+)\.(js)$/ },
-  { module: 'mixin-unit',             exp: /^()tests\/unit\/mixins\/(.+)-test\.(js)$/ },
-  { module: 'mixin-integration',      exp: /^()tests\/integration\/mixins\/(.+)-test\.(js)$/ },
-  { module: 'adapter',                exp: /^(app|addon)\/adapters\/(.+)\.(js)$/ },
-  { module: 'adapter-unit',           exp: /^()tests\/unit\/adapters\/(.+)-test\.(js)$/ },
-  { module: 'adapter-integration',    exp: /^()tests\/integration\/adapters\/(.+)-test\.(js)$/ },
-  { module: 'serializer',             exp: /^(app|addon)\/serializers\/(.+)\.(js)$/ },
-  { module: 'serializer-unit',        exp: /^()tests\/unit\/serializers\/(.+)-test\.(js)$/ },
-  { module: 'serializer-integration', exp: /^()tests\/integration\/serializers\/(.+)-test\.(js)$/ },
-  { module: 'service',                exp: /^(app|addon)\/services\/(.+)\.(js)$/ },
-  { module: 'service-unit',           exp: /^()tests\/unit\/services\/(.+)-test\.(js)$/ },
-  { module: 'service-integration',    exp: /^()tests\/integration\/services\/(.+)-test\.(js)$/ },
+  { module: 'component',               exp: /^(app|addon)\/components\/(.+)\.(js)$/ },
+  { module: 'component-template',      exp: /^(app|addon)\/templates\/components\/(.+)\.(hbs)$/ },
+  { module: 'component-style',         exp: /^(app|addon)\/styles\/components\/(.+)\.(scss)$/ },
+  { module: 'component-unit',          exp: /^()tests\/unit\/components\/(.+)-test\.(js)$/ },
+  { module: 'component-integration',   exp: /^()tests\/integration\/components\/(.+)-test\.(js)$/ },
+  { module: 'route',                   exp: /^(app|addon)\/routes\/(.+)\.(js)$/ },
+  { module: 'route-unit',              exp: /^()tests\/unit\/routes\/(.+)-test\.(js)$/ },
+  { module: 'route-integration',       exp: /^()tests\/integration\/routes\/(.+)-test\.(js)$/ },
+  { module: 'controller',              exp: /^(app|addon)\/controllers\/(.+)\.(js)$/ },
+  { module: 'controller-unit',         exp: /^()tests\/unit\/controllers\/(.+)-test\.(js)$/ },
+  { module: 'controller-integration',  exp: /^()tests\/integration\/controllers\/(.+)-test\.(js)$/ },
+  { module: 'controller-template',     exp: /^(app|addon)\/templates\/(.+)\.(hbs)$/ },
+  { module: 'model',                   exp: /^(app|addon)\/models\/(.+)\.(js)$/ },
+  { module: 'model-unit',              exp: /^()tests\/unit\/models\/(.+)-test\.(js)$/ },
+  { module: 'model-integration',       exp: /^()tests\/integration\/models\/(.+)-test\.(js)$/ },
+  { module: 'util',                    exp: /^(app|addon)\/utils\/(.+)\.(js)$/ },
+  { module: 'util-unit',               exp: /^()tests\/unit\/utils\/(.+)-test\.(js)$/ },
+  { module: 'util-integration',        exp: /^()tests\/integration\/utils\/(.+)-test\.(js)$/ },
+  { module: 'helper',                  exp: /^(app|addon)\/helpers\/(.+)\.(js)$/ },
+  { module: 'helper-unit',             exp: /^()tests\/unit\/helpers\/(.+)-test\.(js)$/ },
+  { module: 'helper-integration',      exp: /^()tests\/integration\/helpers\/(.+)-test\.(js)$/ },
+  { module: 'mixin',                   exp: /^(app|addon)\/mixins\/(.+)\.(js)$/ },
+  { module: 'mixin-unit',              exp: /^()tests\/unit\/mixins\/(.+)-test\.(js)$/ },
+  { module: 'mixin-integration',       exp: /^()tests\/integration\/mixins\/(.+)-test\.(js)$/ },
+  { module: 'adapter',                 exp: /^(app|addon)\/adapters\/(.+)\.(js)$/ },
+  { module: 'adapter-unit',            exp: /^()tests\/unit\/adapters\/(.+)-test\.(js)$/ },
+  { module: 'adapter-integration',     exp: /^()tests\/integration\/adapters\/(.+)-test\.(js)$/ },
+  { module: 'serializer',              exp: /^(app|addon)\/serializers\/(.+)\.(js)$/ },
+  { module: 'serializer-unit',         exp: /^()tests\/unit\/serializers\/(.+)-test\.(js)$/ },
+  { module: 'serializer-integration',  exp: /^()tests\/integration\/serializers\/(.+)-test\.(js)$/ },
+  { module: 'service',                 exp: /^(app|addon)\/services\/(.+)\.(js)$/ },
+  { module: 'service-unit',            exp: /^()tests\/unit\/services\/(.+)-test\.(js)$/ },
+  { module: 'service-integration',     exp: /^()tests\/integration\/services\/(.+)-test\.(js)$/ },
   { module: 'initializer',             exp: /^(app|addon)\/initializers\/(.+)\.(js)$/ },
   { module: 'initializer-unit',        exp: /^()tests\/unit\/initializers\/(.+)-test\.(js)$/ },
   { module: 'initializer-integration', exp: /^()tests\/integration\/initializers\/(.+)-test\.(js)$/ },
@@ -189,9 +189,9 @@ function typeKeyToLabel(typeKey: string) : string {
 }
 
 interface IType {
-  hostType: string,
-  key: string,
-  path: string,
+  hostType: string
+  key: string
+  path: string
   part: string
 }
 
@@ -239,30 +239,26 @@ export function activate(context: vscode.ExtensionContext) {
       return
     }
 
-    try {
-      const relativeFileName = vscode.workspace.asRelativePath(vscode.window.activeTextEditor.document.fileName)
-      const type = detectType(relativeFileName)
+    const relativeFileName = vscode.workspace.asRelativePath(vscode.window.activeTextEditor.document.fileName)
+    const type = detectType(relativeFileName)
 
-      if (!type) { return; }
+    if (!type) { return; }
 
-      const items = getRelatedTypeKeys(type.key)
-        .map((typeKey) => new TypeItem(type, typeKey))
-        .filter((type) => type.exists())
+    const items = getRelatedTypeKeys(type.key)
+      .map((typeKey) => new TypeItem(type, typeKey))
+      .filter((type) => type.exists())
 
-      if (items.length === 0) { return; }
+    if (items.length === 0) { return; }
 
-      if (items.length === 1 && !config('showQuickPickForSingleOption')) {
-        return open(items.pop())
-      }
-      
-      vscode.window.showQuickPick(items, { placeHolder: 'Select File', matchOnDescription: true }).then((item) => {
-        if (item) {
-          open(item)
-        }
-      })
-    } catch(err) {
-      console.error(err.stack);
+    if (items.length === 1 && !config('showQuickPickForSingleOption')) {
+      return open(items.pop())
     }
+    
+    vscode.window.showQuickPick(items, { placeHolder: 'Select File', matchOnDescription: true }).then((item) => {
+      if (item) {
+        open(item)
+      }
+    })
   })
 
   context.subscriptions.push(disposable)
